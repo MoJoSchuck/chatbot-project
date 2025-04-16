3cc6c9a8a4c235b960e90975223080809dfb629c
Moritz Schuck <159003684+MoJoSchuck@users.noreply.github.com>
2025-04-16 10:58:37 +0200

    Add custom responses using Chatbot.addResponses() in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { Chatbot } from 'supersimpledev'
 import { ChatInput } from './components/ChatInput'
-import ChatMessages from './components/ChatMessages';
+import ChatMessages from './components/ChatMessages'
 import './App.css'
 
 function App() {
     const [chatMessages, setChatMessages] = useState([]);
+
+    useEffect(() => {
+        Chatbot.addResponses({
+            'goodbye': 'Goodbye. Have a great day!',
+            'give me a unique id': function() {
+                return `Sure! Here's your unique ID: ${crypto.randomUUID()}`;
+            }
+        })
+    }, []);
+
     return (
         <div className="app-container"> {chatMessages.length === 0 && (
             <p className="welcome-message">
