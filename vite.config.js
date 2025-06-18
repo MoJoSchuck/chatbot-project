17afe349726988509efe8ff4826a0d6601ab7f7f
Moritz Schuck <159003684+MoJoSchuck@users.noreply.github.com>
2025-06-18 11:29:12 +0200

    feat: integrate React Compiler in chatbot project

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
     base: '/chatbot-project/',
-    plugins: [react()],
+    plugins: [react({
+        babel: {
+            plugins: [['babel-plugin-react-compiler', { target: '19' }]],
+        },
+    })],
 })
